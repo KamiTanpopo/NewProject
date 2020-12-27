58498655264559155c790949a1c9766d2d4e6d89
KamiTanpopo <klebasasha@shostka-licey.com>
2020-12-28 01:02:42 +0200

    New-Made

--- MyScript.sh
+++ MyScript.sh
@@ -1,15 +1,6 @@
 
 #!/bin/bash
 
-file1="list.txt"
-for var in $(cat $file1)
-do
-echo " "
-done
-for file2 in /home/tanpopo/dev/proj/NewProject/*
-do
-if [ "$file2" != "$var" ]
-then
-echo "$file2"
-fi
-done
+ls --hide list1.txt > list1.txt
+sort list1.txt list.txt | uniq -u | uniq
+rm list1.txt
